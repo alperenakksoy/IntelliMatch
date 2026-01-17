621b1a5e8953ce62627fba6186a4b1e2b6ee9caf
Ahmet Alperen Aksoy <alperenaksoy611@gmail.com>
2026-01-17 20:47:51 +0300

    Project create

---
+++ src/test/java/com/example/intellimatch/IntellimatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intellimatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellimatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
