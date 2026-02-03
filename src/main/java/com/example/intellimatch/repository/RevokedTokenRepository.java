13477a9f31f8dfe3f89953a5414b2b0711801f59
Ahmet Alperen Aksoy <alperenaksoy611@gmail.com>
2026-02-03 14:24:16 +0300

    Fix bugs

--- src/main/java/com/example/intellimatch/repository/RevokedTokenRepository.java
+++ src/main/java/com/example/intellimatch/repository/RevokedTokenRepository.java
@@ -4,8 +4,6 @@ import com.example.intellimatch.entity.RevokedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RevokedTokenRepository extends JpaRepository<RevokedToken, Long> {
     boolean existsByToken(String token);
